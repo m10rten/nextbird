3d6eec02f5ed5fa3e753467de90ac949731c9585
m10rten <qmaartenenzo@gmail.com>
2023-11-04 15:50:03 +0100

    surpessHydrationWarning

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/* Apply GeistMono and GeistSans as tailwind variable and set GeistSans as default font. */}
       <body
         className={cn(
