5d602d383acdf99139e90a3a160ad4a79d0f9221
m10rten <qmaartenenzo@gmail.com>
2023-11-11 18:52:41 +0100

    Remove unnecessary tests from index.test.ts

--- packages/nextbird/test/index.test.ts
+++ packages/nextbird/test/index.test.ts
@@ -1,17 +1,5 @@
-import nextbird from "../src/next";
-
 describe("nextbird", () => {
   it("should be a test", () => {
     expect(true).toBe(true);
   });
-  it("should be defined", () => {
-    expect(nextbird).toBeDefined();
-  });
-  it("should be a function", () => {
-    expect(typeof nextbird).toBe("function");
-  });
-  // it should return an object with GET method that then handles the GET request.
-  // it("should return an object with GET method that then handles the GET request", () => {
-  //   expect(typeof nextbird().GET).toBe("object");
-  // });
 });
